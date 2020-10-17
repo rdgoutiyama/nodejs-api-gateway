0b13342260cdf1b0e9f0239b79ef21184816bb0c
cmatsubara <71851446+cmatsubara@users.noreply.github.com>
2020-10-17 14:19:38 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,13 +7,13 @@ server
   .then(server => {
     console.log('====================================');
     console.log(
-      `Server its running on port: ${server.application.address().port}`
+      `Server it's running on port: ${server.application.address().port}`
     );
     console.log('====================================');
   })
   .catch(error => {
     console.log('====================================');
-    console.log('Server failed to start');
+    console.log('Server failed to start...');
     console.log('====================================');
     console.error(error);
     process.exit(1);
